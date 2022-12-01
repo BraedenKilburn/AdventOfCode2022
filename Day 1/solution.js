ae14ef9cb1aa417bfa43e1dea810b4ce35651fc4
Braeden Kilburn <braedenk99@gmail.com>
2022-12-01 09:43:08 -0700

    Calorie Challenge complete ✅

--- Day 1/solution.js
+++ Day 1/solution.js
@@ -1,6 +1,7 @@
 // The fs module is required to read files.
 const fs = require('fs')
 
+// File path
 const FILE_PATH = './input.txt'
 
 // Read the input file.
@@ -9,16 +10,22 @@ const input = fs.readFileSync(FILE_PATH, 'utf8')
 // Trim and Split the input into an array of elves.
 // An elf is an array of some number of integers.
 // We'll then reduce each elves array into a sum of its integers.
+// Sort the elves from greatest to smallest.
+// Then select only the top three elves.
 const elves = input
   .trim()
   .split('\n\n')
   .map((elf) => elf.split('\n').map((line) => parseInt(line)))
   .map((elf) => elf.reduce((a, b) => a + b))
+  .sort((a, b) => b - a)
+  .splice(0, 3)
 
-// Now, we need to find the largest number in the elves array.
-// We'll use the spread operator to pass the array as arguments to Math.max.
-const largest = Math.max(...elves)
+// The total calories of the top three elves.
+const total = elves.reduce((a, b) => a + b)
 
 console.log(
-  `🎄 The most calories available is ${largest.toLocaleString()} calories`,
+  `🎄 The most calories available from one elf is ${elves[0].toLocaleString()} calories.`,
+)
+console.log(
+  `🎄 The total calories between the top three elves is ${total.toLocaleString()} calories.`,
 )
